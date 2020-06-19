96e043c8caf208aa24b04ca5a864f9ce903c0aa8
ganesh1172 <ganesh172595@gmail.com>
2020-06-19 16:47:09 +0530

    write function to switch dark or light theme

---
+++ src/useDarkMode.js
@@ -0,0 +1,23 @@
+import { useEffect, useState } from 'react';
+import { light as LightTheme, dark as DarkTheme } from './style';
+
+export const useDarkMode = () => {
+    const [theme, setTheme] = useState(LightTheme);
+
+    const toggleTheme = () => {
+        if (theme.id === 'light') {
+            window.localStorage.setItem('theme', 'dark')
+            setTheme(DarkTheme)
+        } else {
+            window.localStorage.setItem('theme', 'light')
+            setTheme(LightTheme)
+        }
+    };
+
+    useEffect(() => {
+        const localTheme = window.localStorage.getItem('theme');
+        (localStorage === 'light') ? localTheme && setTheme(LightTheme) : localTheme && setTheme(DarkTheme);
+    }, []);
+
+    return [theme, toggleTheme]
+}
