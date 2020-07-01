0acd916b7bd0d91f75155d6a4ba461434c92101b
ganesh1172 <ganesh172595@gmail.com>
2020-07-01 23:20:21 +0530

    create new page userprofile and import  header, loader components

---
+++ src/pages/UserProfile.js
@@ -0,0 +1,16 @@
+import React from 'react';
+// import styled from "styled-components";
+import { Header, Loader } from '../components';
+// import github from "../api/github";
+// import LanguageContext from '../contexts/LanguageContext';
+
+const UserProfile = (props) => {
+    return (
+        <>
+            <Header />
+            <Loader />
+        </>
+    )
+}
+
+export default UserProfile;
