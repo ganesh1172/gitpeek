1766ca0937923551e752810642b7711f9754deab
ganesh1172 <ganesh172595@gmail.com>
2020-07-01 23:34:26 +0530

    comment unwanted code

--- src/api/gitapi.js
+++ src/api/gitapi.js
@@ -1,6 +1,12 @@
 import axios from 'axios';
 
+// let githubClientId;
+// let githubClientSecret;
+
 export default axios.create({
     baseURL: "https://api.github.com",
-
+    // auth: {
+    //     username: githubClientId,
+    //     password: githubClientSecret,
+    // }
 });
