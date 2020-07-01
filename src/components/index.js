51f0b78c3558bef78b25520ed18b079d2215d9e7
ganesh1172 <ganesh172595@gmail.com>
2020-07-01 23:27:30 +0530

    form, header, loader export from index.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,8 @@
 import Logo from './Logo';
 import Toggle from './Toggle';
 import Button from './Button';
+import Form from './Form';
+import Header from './Header';
+import Loader from './Loader';
 
-export { Logo, Toggle, Button };
+export { Logo, Toggle, Button, Form, Header, Loader };
