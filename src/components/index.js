90b067e7308158459d558173be620167c49f6f5d
ganesh1172 <ganesh172595@gmail.com>
2020-07-02 23:38:50 +0530

    export error component

--- src/components/index.js
+++ src/components/index.js
@@ -4,5 +4,6 @@ import Button from './Button';
 import Form from './Form';
 import Header from './Header';
 import Loader from './Loader';
+import Error from './Error';
 
-export { Logo, Toggle, Button, Form, Header, Loader };
+export { Logo, Toggle, Button, Form, Header, Loader, Error };
