ba6da7c56f9a6cafa5a0594850a0d2d8e6788b84
ganesh1172 <ganesh172595@gmail.com>
2020-07-04 23:41:46 +0530

    removed activity data and component from userProfile

--- src/pages/UserProfile.js
+++ src/pages/UserProfile.js
@@ -1,17 +1,13 @@
 import React, { useState, useEffect } from 'react';
-// import styled from "styled-components";
-import { Header, Loader, Error, Profile, Activities } from '../components';
+import { Header, Loader, Error, Profile } from '../components';
 import LanguageContext from '../contexts/LanguageContext';
-// import Profile from '../components/Profile';
 import github from "../api/gitapi";
-// import Activities from '../components/Activities';
 
 const UserProfile = (props) => {
     const username = props.match.params.id;
     const [userData, setUserData] = useState({});
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState({});
-    const [activityData, setActivityData] = useState([]);
 
     const getUserData = async () => {
         setLoading(true);
@@ -40,34 +36,9 @@ const UserProfile = (props) => {
         }
     }
 
-    const getActivityData = async () => {
-        setLoading(true);
-
-        try {
-            const response = await github.get(`/users/${username}/events?per_page=30`);
-
-            setActivityData(response.data);
-            setLoading(false);
-        } catch (error) {
-            if (error.response) {
-                if (error.response.status === 404) {
-                    setError({ active: true, type: 404 });
-                } else {
-                    setError({ active: true, type: error.response.status });
-                }
-            } else {
-                setError({ active: true, type: error });
-                console.log(error);
-            }
-
-            setLoading(false);
-        }
-    }
-
     useEffect(() => {
         setError({ active: false, type: 200 });
         getUserData();
-        getActivityData();
     }, [username]);
 
     if (loading) {
@@ -88,7 +59,6 @@ const UserProfile = (props) => {
                     ) : (
                             <LanguageContext.Provider>
                                 <Profile userData={userData} />
-                                <Activities activityData={activityData} />
                             </LanguageContext.Provider>
                         )}
                 </main>
