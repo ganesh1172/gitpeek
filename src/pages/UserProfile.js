caef7a79e37b6cbddd567d9aea97f91b4b77001c
ganesh1172 <ganesh172595@gmail.com>
2020-07-04 10:41:56 +0530

    fetch activities data through async

--- src/pages/UserProfile.js
+++ src/pages/UserProfile.js
@@ -1,15 +1,17 @@
 import React, { useState, useEffect } from 'react';
 // import styled from "styled-components";
-import { Header, Loader, Error } from '../components';
+import { Header, Loader, Error, Profile, Activities } from '../components';
 import LanguageContext from '../contexts/LanguageContext';
-import Profile from '../components/Profile';
+// import Profile from '../components/Profile';
 import github from "../api/gitapi";
+// import Activities from '../components/Activities';
 
 const UserProfile = (props) => {
     const username = props.match.params.id;
     const [userData, setUserData] = useState({});
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState({});
+    const [activityData, setActivityData] = useState([]);
 
     const getUserData = async () => {
         setLoading(true);
@@ -38,9 +40,34 @@ const UserProfile = (props) => {
         }
     }
 
+    const getActivityData = async () => {
+        setLoading(true);
+
+        try {
+            const response = await github.get(`/users/${username}/events?per_page=30`);
+
+            setActivityData(response.data);
+            setLoading(false);
+        } catch (error) {
+            if (error.response) {
+                if (error.response.status === 404) {
+                    setError({ active: true, type: 404 });
+                } else {
+                    setError({ active: true, type: error.response.status });
+                }
+            } else {
+                setError({ active: true, type: error });
+                console.log(error);
+            }
+
+            setLoading(false);
+        }
+    }
+
     useEffect(() => {
         setError({ active: false, type: 200 });
         getUserData();
+        getActivityData();
     }, [username]);
 
     if (loading) {
@@ -61,6 +88,7 @@ const UserProfile = (props) => {
                     ) : (
                             <LanguageContext.Provider>
                                 <Profile userData={userData} />
+                                <Activities activityData={activityData} />
                             </LanguageContext.Provider>
                         )}
                 </main>
