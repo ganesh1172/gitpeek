62852a6650fe8ebc4d8b3721de2844355e35f9a0
ganesh1172 <ganesh172595@gmail.com>
2020-07-04 23:47:34 +0530

    chagend in background-color, paddings and remove transform property

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,18 +3,16 @@ import styled from 'styled-components';
 const Button = styled.button`
     color: rgb(255,255,255);
     border: none;
-    // background-image: linear-gradient(to right, #01F2FC,#1787CF);
-    background: #5c75f6;
-    padding: 0.7rem 2rem;
-    border-radius: 5px;
+    background: #1F8BDB;
+    padding: 0.5rem 0.5rem;
+    border-radius: 50%;
     border-bottom: 2px solid transparent;
     cursor: pointer;
     align-items: center;
     transition: all 0.3s;
     
     &:hover {
-        transform: scale(1.1);
-        box-shadow: 0 1rem 2rem 0 rgba(0,0,0, .2);
+        box-shadow: 0 1rem 2rem 0 rgba(0,0,0,0.2);
     }
     &:focus {
         outline: 0;
