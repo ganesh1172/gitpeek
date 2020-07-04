e31cff6a3ceb916489bbb2a8331dc2cb3f5a3231
ganesh1172 <ganesh172595@gmail.com>
2020-07-04 10:40:36 +0530

    change in background color

--- src/style/light.js
+++ src/style/light.js
@@ -1,6 +1,6 @@
 const theme = {
     id: 'light',
-    bgColor: "#F5F6FA",
+    bgColor: "#fff",
     cardColor: "rgb(255,255,255)",
     inputColor: "rgba(238,238,238, 0.8)",
     textColor: "rgb(85, 85, 85)",
